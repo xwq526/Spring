5a60a89bec4574e833d0cc33fb43b454314297de
xwq <1561493487@qq.com>
2021-01-13 20:51:46 +0800

    SpringMVC学习

--- src/main/java/org/example/controller/TestArgumentController.java
+++ src/main/java/org/example/controller/TestArgumentController.java
@@ -1,9 +1,12 @@
 package org.example.controller;
 
+import org.example.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -46,4 +49,60 @@ public class TestArgumentController {
         map.put("ok",true);
         return map;
     }
+    @PostMapping("/register")
+    public Object register(@RequestParam String username,
+                           @RequestParam String password,
+                           @RequestParam MultipartFile file) throws IOException {
+        log.debug("获取到请求路径参数: username={},password={}",username,password);
+        log.debug("头像信息，名称={},内容={}",file.getOriginalFilename(),new String(file.getBytes()));
+        Map<String,Object> map = new HashMap<>();
+        map.put("ok",true);
+        return map;
+    }
+
+
+
+    @PostMapping("/login/pojo1")
+    public Object loginPojo1( String username, String password) {
+        log.debug("获取到请求路径参数: username={},password={}",username,password);
+        Map<String,Object> map = new HashMap<>();
+        map.put("ok",true);
+        return map;
+    }
+
+    //springmvc自动实例化user对象，根据请求key获取值，注入到user对象的属性中
+    @PostMapping("/login/pojo2")
+    public Object loginPojo2(User user) {
+        log.debug("获取到请求路径参数: username={},password={},i={}",user.getUsername(),user.getPassword(),user.getI());
+        Map<String,Object> map = new HashMap<>();
+        map.put("ok",true);
+        return map;
+    }
+
+    @PostMapping("/register/pojo")
+    public Object register(User user,MultipartFile file) throws IOException {
+        log.debug("获取到请求路径参数: username={},password={}",user.getUsername(),user.getPassword());
+        log.debug("头像信息，名称={},内容={}",file.getOriginalFilename(),new String(file.getBytes()));
+        Map<String,Object> map = new HashMap<>();
+        map.put("ok",true);
+        return map;
+    }
+
+    @PostMapping("/json")
+    public Object json(@RequestBody User user) {
+        log.debug("用户信息为：{}",user);
+        Map<String,Object> map = new HashMap<>();
+        map.put("ok",true);
+        return map;
+    }
+
+    @PostMapping("/file")
+    public Object file(@RequestPart MultipartFile file) throws IOException {
+        log.debug("头像信息，名称={}, 内容={}", file.getOriginalFilename(),
+                new String(file.getBytes()));
+        Map<String, Object> map = new HashMap<>();
+        map.put("ok", true);
+        return map;
+    }
+
 }
---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,21 @@
+package org.example.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xuwenqing
+ * Date: 2021-01-12
+ * Time: 23:53:53
+ */
+@Setter
+@Getter
+@ToString
+public class User {
+    private String username;
+    private String password;
+    private Integer i;
+}
